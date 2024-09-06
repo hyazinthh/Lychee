d9586f182ea7d7ef09c2aae0d9bf331e090668e1
Martin <martin.m@mailbox.org>
2024-09-06 14:07:42 +0000

    Send mails twice a day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends ConsoleKernel
 	 */
 	protected function schedule(Schedule $schedule): void
 	{
-		$schedule->command('lychee:photos_added_notification')->dailyAt('02:55');
+		$schedule->command('lychee:photos_added_notification')->twiceDaily(1, 13);
 	}
 
 	/**
