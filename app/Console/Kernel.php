af9e67883b2d5a6bf08e9eb5d76747eb8d4c19b8
Martin <mautner@vrvis.at>
2026-03-22 14:55:32 +0100

    Send emails twice per day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
 	 */
 	protected function schedule(Schedule $schedule): void
 	{
-		$schedule->command('lychee:photos_added_notification')->weekly();
+		$schedule->command('lychee:photos_added_notification')->twiceDaily(1, 13);
 	}
 
 	/**
